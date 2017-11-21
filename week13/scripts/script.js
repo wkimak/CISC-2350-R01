cc8370fff8b4c9a85e7c41e5c5401f8aa6844a76
William Kimak <31807815+wkimak@users.noreply.github.com>
2017-11-20 23:04:14 -0500

    Update script.js

--- week13/scripts/script.js
+++ week13/scripts/script.js
@@ -3,11 +3,13 @@
 var nav = document.getElementById("nav");
 var body=document.querySelector("body");
 var picsContainer = document.getElementsByClassName("container_12")[0];
+var apiContainer = document.getElementsByClassName("apiContainer")[0];
 var themesDiv = document.getElementById("themes");
 var theme1 = document.getElementById("theme1");
 var theme2 = document.getElementById("theme2");
 var theme3 = document.getElementById("theme3");
 var foodImages = document.getElementsByClassName("foodImage");
+var ul = document.getElementById("ajaxPhotos");
 
 
 
@@ -44,45 +46,46 @@ following.innerHTML = followingNum + "<br> <b> Following </b>";
 /* ----------- Themes Buttons -------------- */
 themesDiv.addEventListener("click", function(event){
 if(event.target == theme1){
+  //set these container displays back to default
+  apiContainer.style.display="";
+  picsContainer.style.display="";
+  //add picsContainer animation
   picsContainer.classList.add("animateContainer");
- 
-//always set image display back to block
- for(var i = 0; i<foodImages.length; i++){
-    foodImages[i].style.display = "";
-  }
-
+  //remove class after 3 seconds so animation can happen again
   setTimeout(function(){
     picsContainer.classList.remove("animateContainer");
   },3000);	
+  //new theme colors
   nav.style.backgroundColor = "";
   body.style.backgroundColor = "";
   picsContainer.style.backgroundColor = "";
 } else if (event.target == theme2){
-	picsContainer.classList.add("animateContainer");
-
-//always set image display back to block
-  for(var i = 0; i<foodImages.length; i++){
-    foodImages[i].style.display = "";
-  }
- 
-	setTimeout(function(){
+  apiContainer.style.display="";
+  picsContainer.style.display="";
+	
+  picsContainer.classList.add("animateContainer");
+  setTimeout(function(){
     picsContainer.classList.remove("animateContainer");
   },3000);	
+
 	nav.style.backgroundColor = "green";
 	body.style.backgroundColor = "black";
 	picsContainer.style.backgroundColor = "white";
 } else if (event.target == theme3){
-	picsContainer.classList.add("animateContainer");
+  //set picsContainer to none so I can add in new container for Flickr images
+  picsContainer.style.display="none";
+  apiContainer.style.display="block";
+
+	apiContainer.classList.add("animateContainer");
 	setTimeout(function(){
-    picsContainer.classList.remove("animateContainer");
+    apiContainer.classList.remove("animateContainer");
   },3000);	
+
 	nav.style.backgroundColor = "white";
 	body.style.backgroundColor = "blue";
-	picsContainer.style.backgroundColor = "lightblue";
+	apiContainer.style.backgroundColor = "lightblue";
 
-//always set image display to none to leave room for AJAX
-  for(var i = 0; i<foodImages.length; i++){
-    foodImages[i].style.display = "none";
-  }
+//always set image display to none to leave room for AJAX  
 }
 });
+
